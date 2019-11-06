91f356270545232670eb9c61c00bc884ef8345d2
eastclo <goldeastcloud@gmail.com>
2019-11-06 23:05:41 +0900

    [Model] Add Command List 구현 된 깃 명령어들을 저장하는 CommandList 초안입니다.

---
+++ GitPractice/src/Model/CommandList.java
@@ -0,0 +1,22 @@
+package Model;
+
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+
+public class CommandList {
+	private String commands[];
+	
+	public CommandList() {
+		/*cmdlist 폴더에 각각의 명령어 이름으로 파일 생성하여 저장*/
+		File f = new File(".\\src\\Model\\cmdlist");
+		this.commands = f.list();
+		
+		/*Model하위 commandlist 파일 안에 명령어 목록 작성*/
+//		FileReader fin = new FileReader(".\\commandlist.txt");
+	}
+	
+	public String[] getCommandList() {
+		return commands;
+	}
+}
