0f61a801020281d068af7f5abcc9fee8fcc430b2
eastclo <goldeastcloud@gmail.com>
2019-11-06 23:09:27 +0900

    [Controller] CommandInputListener 동작 확인을 위한 테스트 클래스 파일입니다.

---
+++ GitPractice/src/Controller/ExecutionLogOptionAll.java
@@ -0,0 +1,7 @@
+package Controller;
+
+public class ExecutionLogOptionAll {
+	public void executeCommand() {
+		System.out.println("I'm git log --all");
+	}
+}
---
+++ GitPractice/src/Controller/ExecutionLogOptionAllGraph.java
@@ -0,0 +1,7 @@
+package Controller;
+
+public class ExecutionLogOptionAllGraph {
+	public void executeCommand() {
+		System.out.println("I'm git log --all --graph");
+	}
+}
