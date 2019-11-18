654a70dcbc2d38cf533377f7f144cfb2e7179c4c
eastclo <goldeastcloud@gmail.com>
2019-11-18 16:28:17 +0900

    [Controller] 뒤로가기, 앞으로가기 리스너 구현 CommandStack.java	-> 스택 모델 구현 GobackButtonListener.java	->	리스너 구현 CommandInputListener.java	->	명령어 입력시 스택에 저장하는 부분 추가 CommandInputPane.java	->	버튼에 리스너 적용 ExecutionLogOptionAll.java	-> 테스트용 cancel메소드 추가 ExecutionLogOptionAllGraph.java	-> 테스트용 cancel메소드 추가

--- GitPractice/src/Controller/ExecutionLogOptionAll.java
+++ GitPractice/src/Controller/ExecutionLogOptionAll.java
@@ -4,4 +4,7 @@ public class ExecutionLogOptionAll {
 	public void executeCommand() {
 		System.out.println("I'm git log --all");
 	}
+	public void cancelCommand() {
+		System.out.println("Cancel git log --all");
+	}
 }
