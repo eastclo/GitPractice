4809dd829699494d6846df7c2ece942b11866a9f
Jeongmin Choi <jmgraming@gmail.com>
2019-11-20 17:08:35 +0900

    fix1 : 창의 비율을 자동으로 설정하는 클래스 생성

---
+++ GitPractice/src/View/AutomaticSizeFrame.java
@@ -0,0 +1,25 @@
+package View;
+
+import javax.swing.JFrame;
+import javax.swing.text.View;
+
+import javafx.scene.Scene;
+import javafx.scene.layout.StackPane;
+
+public class AutomaticSizeFrame extends JFrame { //창의 비율을 자동으로 설정하는 클래스
+
+	//Test
+	StackPane pane = new StackPane();
+	Scene scene = new Scene(pane, 500, 500);
+	
+		/*
+	* 
+	* View auto = new View();
+	* auto.setPreserveRatio(true);
+	* auto.setFitWidth(1024);
+	* 
+	* pane.getChilderen().add(auto);
+	* 
+		*/
+	
+}
