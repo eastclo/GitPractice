c3d72ac8ed9cc27767baca2bc06528d9618fa545
Jeongmin Choi <jmgraming@gmail.com>
2019-11-25 15:56:24 +0900

    콤보박스 추가, 콤보박스 리스트 추가, upload 버튼 크기 수정

--- GitPractice/src/View/CommandInputPane.java
+++ GitPractice/src/View/CommandInputPane.java
@@ -1,5 +1,7 @@
 package View;
 
+import java.util.List;
+import java.util.LinkedList;
 import java.awt.*;
 import java.io.File;
 
@@ -8,10 +10,17 @@ import javax.swing.*;
 
 import Controller.CommandInputListener;
 import Controller.GobackButtonListener;
+import java.awt.event.WindowEvent;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import javax.swing.JButton;
+import javax.swing.JFrame;
 
 
 public class CommandInputPane extends JPanel{
 	
+	private static final String EXIT_ON_CLOSE = null;
+	private JButton btnBack; 
 	public CommandInputPane(){
 		JPanel CommandInputPane = new JPanel();
 		CommandInputPane.setBounds(236, 0, 539, 409);
@@ -32,21 +41,24 @@ public class CommandInputPane extends JPanel{
 		CommandInputPane.add(allCommandtxt);
 		
 		JTextField input = new JTextField();
-		input.setBounds(14, 373, 442, 24);
+		input.setBounds(14, 373, 429, 24);
 		CommandInputPane.add(input);
 		input.addActionListener(new CommandInputListener()); //리스너 적용
 		
-		ImageIcon BackIcon = new ImageIcon("."+File.separator+"..\\res\\back1.png");
+		ImageIcon BackIcon = new ImageIcon("."+File.separator+".."+File.separator+"res"+File.separator+"back1.png");
 		JButton btnBack = new JButton(BackIcon);//뒤로가기 버튼 이미지 추가
 		btnBack.setName("back");	//리스너 내부에서 뒤로가기 버튼임을 인식하기 위한 name 설정
 		btnBack.setBorderPainted(false);
 		btnBack.setContentAreaFilled(false);
 		btnBack.setBounds(372, 11, 90, 20);
 		btnBack.addActionListener(new GobackButtonListener());	//리스너 적용
-
-		CommandInputPane.add(btnBack);
 		
-		ImageIcon ForwardIcon = new ImageIcon("."+File.separator+"..\\res\\forward1.png"); //앞으로가기 버튼 추가
+		CommandInputPane.add(btnBack);
+		btnBack.addActionListener(new buttonPress());
+		btnBack.setVisible(true);
+		//btnBack.setDefaultCloseOperation(EXIT_ON_CLOSE);
+		
+		ImageIcon ForwardIcon = new ImageIcon("."+File.separator+".."+File.separator+"res"+File.separator+"forward1.png"); //앞으로가기 버튼 추가
 		JButton btnForward = new JButton(ForwardIcon);//뒤로가기 버튼 이미지 추가
 		btnForward.setName("forward");	//리스너 내부에서 앞으로가기 버튼임을 인식하기 위한 name 설정
 		btnForward.setBorderPainted(false);
@@ -56,9 +68,26 @@ public class CommandInputPane extends JPanel{
 		CommandInputPane.add(btnForward);
 		
 		JButton btnUpload = new JButton("upload");
-		btnUpload.setBounds(460, 371, 65, 26);
+		btnUpload.setBounds(448, 372, 77, 26);
 		CommandInputPane.add(btnUpload);
-	
+		
+		JComboBox<List> brcomboBox = new JComboBox<List>();
+		brcomboBox.setBounds(201, 9, 100, 24);
+		CommandInputPane.add(brcomboBox);
+		List branchName = new LinkedList(); //콤보박스에 들어갈 내용을 저장하는 리스트
+		branchName.add("master"); 			//master 추가, 추후에 checkout 명령어 입력시 branch가 추가되도록 해야할 것
 
+		JLabel lblSelect = new JLabel("브랜치 선택"); //브랜치를 선택하는 라벨 추가
+		lblSelect.setBounds(119, 12, 83, 18); 
+		CommandInputPane.add(lblSelect);
+				
 	}
-}
+	
+		 class buttonPress implements ActionListener {
+		        @Override
+		        public void actionPerformed(ActionEvent arg0) {
+		            CommandInputPane  pF = new CommandInputPane();
+		          // btnBack.dispose();
+		    }
+		 }	
+	 }
