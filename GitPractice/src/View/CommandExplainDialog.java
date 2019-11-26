fe925a3216558564c121b38819a00687aaf49ed7
Jeongmin Choi <jmgraming@gmail.com>
2019-11-26 18:55:34 +0900

    줄바꿈 문제 해결, 설명창 배경색 light grey 설정

--- GitPractice/src/View/CommandExplainDialog.java
+++ GitPractice/src/View/CommandExplainDialog.java
@@ -1,23 +1,37 @@
 package View;
 
 import java.awt.BorderLayout;
+import java.awt.Color;
 import java.awt.Dimension;
 
 import javax.swing.*;
+import java.awt.FlowLayout;
 
 public class CommandExplainDialog extends JFrame {
-	private JLabel jlb = new JLabel("");
+	
+	private JTextArea explainArea = new JTextArea("");
 	
 	public CommandExplainDialog(String str) {
-		JPanel pane = new JPanel();
-		pane.add(jlb);
-		pane.setBorder(BorderFactory.createEmptyBorder(10 , 10 , 10 , 10));	//내부 패딩 설정
-		jlb.setText(str.toString());
 		
-		this.add(pane, BorderLayout.WEST);
-		this.setMinimumSize(new Dimension(500,200));	//최소 크기 설정
+		JPanel pane = new JPanel();
+		FlowLayout flowLayout = (FlowLayout) pane.getLayout();
+        pane.setBackground(Color.LIGHT_GRAY);
+		explainArea.setEditable(false);
+		explainArea.setBackground(Color.LIGHT_GRAY);
+		pane.add(explainArea);
+		pane.setBorder(BorderFactory.createEmptyBorder(10 , 10 , 10 , 10));	//내부 패딩 설정
+		explainArea.setText(str.toString());
+		
+		getContentPane().add(pane, BorderLayout.WEST);
+		
+		JScrollBar explainScrollBar = new JScrollBar();
+		getContentPane().add(explainScrollBar, BorderLayout.EAST);
+		//explainScrollBar.setBounds(10, 40, 13, 325);
+
 		this.pack();
 		this.setLocation(200,200);
+		//this.setMaximumSize(new Dimension(30,20));
 		this.setVisible(true);
 	}
+
 }
