5c12feb27bbf468d8e97a93324e578bded9eaee5
eastclo <goldeastcloud@gmail.com>
2019-12-03 21:56:16 +0900

    명령어 입력 컨트롤러 수정 CommandInputListener.java -> 1. 깃 레포지토리가 아닌 상태에서 git init, git clone이 아닌 명령어를 실행하면 실행되지 않게 예외처리 2. 명령어가 성공적으로 실행되면 입력한 텍스트필드가 비워지게 수정.

--- GitPractice/src/Controller/CommandInputListener.java
+++ GitPractice/src/Controller/CommandInputListener.java
@@ -20,6 +20,7 @@ public class CommandInputListener implements ActionListener {
 
 	@Override
 	public void actionPerformed(ActionEvent e) {
+		boolean check = true;
 		//textfield로부터 입력 값을 가져온다.
 		JTextField txtField = (JTextField) e.getSource();
 		String textFieldValue = txtField.getText();
@@ -37,20 +38,29 @@ public class CommandInputListener implements ActionListener {
 			for(int i=0; i<cmds.length; i++) {
 				cmdsDevide[i] = CommandListOperation.devideInputText(cmds[i]);
 			}
+			if(!(new File(Model.CurrentLocation.workspace,".git").exists())) {
+				if(!input[1].equals("clone") && !input[1].equals("init")) {
+					JOptionPane.showMessageDialog(null, "깃 명령어를 사용할 수 없습니다. 깃 레포지토리로 초기화한 후 사용하시기 바랍니다.", "명령어 입력 오류", JOptionPane.ERROR_MESSAGE);
+					check = false;
+				}
+			}
+			if(check) {
+				int index = CommandListOperation.searchCommand(cmdsDevide, input); //배열에 입력된 명령어가 있는지 검색
 				
-			int index = CommandListOperation.searchCommand(cmdsDevide, input); //배열에 입력된 명령어가 있는지 검색
-			
-			if(index == -1) {	/**예외처리: 입력한 명령어가 없으면 오류 출력*/
-				JOptionPane.showMessageDialog(null, "없는 명령어입니다.", "명령어 입력 오류", JOptionPane.ERROR_MESSAGE);
-			} else {				
-				//명령어를 파일 이름으로 저장했으므로 해당 명령어파일 내부에 적힌 클래스 이름을 얻기 위해 파일 객체와  FileReader를 사용함.
-				String cmdlistPath = "." + File.separator + "src" + File.separator + "Model" + File.separator + "cmdlist";
-				String Clazz = FileOperation.getFileReadData(cmdlistPath, cmds, index);	//파일 내부 데이터를 읽어오는 메소드 호출
-		        
-		        //명렁어 실행
-				JOptionPane.showMessageDialog(null, textFieldValue.trim().replaceAll(" +", " ") + " 명령어를 수행합니다.", "명령어 입력", JOptionPane.INFORMATION_MESSAGE);
-				if(CommandListOperation.execute("executeCommand",Clazz, input))
-					Model.CommandStack.push(textFieldValue);
+				if(index == -1) {	/**예외처리: 입력한 명령어가 없으면 오류 출력*/
+					JOptionPane.showMessageDialog(null, "없는 명령어입니다.", "명령어 입력 오류", JOptionPane.ERROR_MESSAGE);
+				} else {				
+					//명령어를 파일 이름으로 저장했으므로 해당 명령어파일 내부에 적힌 클래스 이름을 얻기 위해 파일 객체와  FileReader를 사용함.
+					String cmdlistPath = "." + File.separator + "src" + File.separator + "Model" + File.separator + "cmdlist";
+					String Clazz = FileOperation.getFileReadData(cmdlistPath, cmds, index);	//파일 내부 데이터를 읽어오는 메소드 호출
+			        
+			        //명렁어 실행
+					JOptionPane.showMessageDialog(null, textFieldValue.trim().replaceAll(" +", " ") + " 명령어를 수행합니다.", "명령어 입력", JOptionPane.INFORMATION_MESSAGE);
+					if(CommandListOperation.execute("executeCommand",Clazz, input))
+						Model.CommandStack.push(textFieldValue);
+					JTextField textField = View.CommandInputPane.getTextField();
+					textField.setText("");
+				}
 			}
 		}
 	}
