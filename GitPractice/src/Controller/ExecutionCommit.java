014dde5abb81bdbce01df0cab4d0cde2de446817
YunSangsoo <yss6024@naver.com>
2019-12-04 19:43:38 +0900

    fix
    ExecutionCommit.java -> 저장방식변경
    ExecutionLog.java -> 로그 출력방법 수정

--- GitPractice/src/Controller/ExecutionCommit.java
+++ GitPractice/src/Controller/ExecutionCommit.java
@@ -4,6 +4,8 @@ import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import java.io.File;
 import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
 
 import javax.swing.JButton;
 
@@ -44,6 +46,16 @@ public class ExecutionCommit {
 			}
 			commit.commitAdd(inputContent,CurrentLocation.getBranch(),CurrentLocation.AuthorName,CurrentLocation.AuthorAddress);
 			commit.workspaceCopy(new File(CurrentLocation.workspace.getPath()+File.separator+".git"+File.separator+"add"),new File(CurrentLocation.workspace.getPath()+File.separator+CurrentLocation.getBranch()+File.separator+"commit"));
+
+			List<String> branchList = commit.BranchArray;
+			List<Integer> check=new ArrayList<Integer>();
+			for(int i=0;i<branchList.size();i++)
+			{
+				if(branchList.get(i).equals(CurrentLocation.getBranch()))
+					check.add(i);
+			}
+			if(check.size()>1)
+				commit.workspaceCopy(new File(CurrentLocation.workspace.getPath()+File.separator+CurrentLocation.getBranch()+File.separator+"commit"+File.separator+check.get(check.size()-1)), new File(CurrentLocation.workspace.getPath()+File.separator+CurrentLocation.getBranch()+File.separator+"commit"));
 			FileOperation.deleteFile(new File(CurrentLocation.workspace.getPath()+File.separator+".git"+File.separator+"add"));
 			commit.commitListSave(commit.Path);
 		}
--- GitPractice/src/Controller/ExecutionLog.java
+++ GitPractice/src/Controller/ExecutionLog.java
@@ -9,6 +9,7 @@ import javax.swing.JOptionPane;
 
 import org.json.simple.parser.ParseException;
 
+import Model.CurrentLocation;
 import View.CommandInputPane;
 
 public class ExecutionLog {
@@ -27,14 +28,95 @@ public class ExecutionLog {
 			List<String> content = new ArrayList<String>();
 			List<String> Authorname=new ArrayList<String>();
 			List<String> AuthorAddress=new ArrayList<String>();
+			List<String> branchList = new ArrayList<String>();
 			content = openCommit.CMArray;
 			Authorname=openCommit.AuthorNameArray;
 			AuthorAddress=openCommit.AuthorAddressArray;
-			for(int i=0;i<content.size();i++)
+			branchList=openCommit.BranchArray;
+			int check=0;
+			for(int i=0;i<branchList.size();i++)
+			{
+				if(branchList.get(i).equals(CurrentLocation.getBranch()))
+				{
+					check=i;
+					break;
+				}
+			}
+			for(int i=0;i<check;i++)
 			{
 				logtext = logtext+"commit " + Integer.toString(i) + "\n";
 				logtext = logtext + "Author: "+Authorname.get(i) +" <"+AuthorAddress.get(i) +">\n";
 				logtext = logtext+"\n" + "\t" + content.get(i)+"\n\n";
+			}
+			for(int i=check;i<content.size();i++)
+			{
+				if(branchList.get(i).equals(CurrentLocation.getBranch()))
+				{
+					logtext = logtext+"commit " + Integer.toString(i) + "\n";
+					logtext = logtext + "Author: "+Authorname.get(i) +" <"+AuthorAddress.get(i) +">\n";
+					logtext = logtext+"\n" + "\t" + content.get(i)+"\n\n";
+				}
+			}
+			CommandInputPane.allCommandtxt.setText(logtext);
+			return true;
+		}
+		else if(parameter.length==1)
+		{
+			boolean cnt=false;
+			for(int i=0;i<CurrentLocation.getBranchList().size();i++)
+			{
+				if(parameter[0].equals(CurrentLocation.getBranchList().get(i)))
+				{
+					cnt=true;
+					break;
+				}
+			}
+			if(cnt==false)
+			{
+				JOptionPane.showMessageDialog(null, "유효하지 않은 인자입니다.", "명령어 입력 오류", JOptionPane.ERROR_MESSAGE);
+				return false;
+			}
+				
+			String logtext = new String();
+			CommitFunction openCommit = new CommitFunction();
+			try {
+				openCommit.commitListOpen();
+			} catch (IOException e) {
+				e.printStackTrace();
+			} catch (ParseException e) {
+				e.printStackTrace();
+			}
+			List<String> content = new ArrayList<String>();
+			List<String> Authorname=new ArrayList<String>();
+			List<String> AuthorAddress=new ArrayList<String>();
+			List<String> branchList = new ArrayList<String>();
+			content = openCommit.CMArray;
+			Authorname=openCommit.AuthorNameArray;
+			AuthorAddress=openCommit.AuthorAddressArray;
+			branchList=openCommit.BranchArray;
+			int check=0;
+			for(int i=0;i<branchList.size();i++)
+			{
+				if(branchList.get(i).equals(parameter[0]))
+				{
+					check=i;
+					break;
+				}
+			}
+			for(int i=0;i<check;i++)
+			{
+				logtext = logtext+"commit " + Integer.toString(i) + "\n";
+				logtext = logtext + "Author: "+Authorname.get(i) +" <"+AuthorAddress.get(i) +">\n";
+				logtext = logtext+"\n" + "\t" + content.get(i)+"\n\n";
+			}
+			for(int i=check;i<content.size();i++)
+			{
+				if(branchList.get(i).equals(parameter[0]))
+				{
+					logtext = logtext+"commit " + Integer.toString(i) + "\n";
+					logtext = logtext + "Author: "+Authorname.get(i) +" <"+AuthorAddress.get(i) +">\n";
+					logtext = logtext+"\n" + "\t" + content.get(i)+"\n\n";
+				}
 			}
 			CommandInputPane.allCommandtxt.setText(logtext);
 			return true;
