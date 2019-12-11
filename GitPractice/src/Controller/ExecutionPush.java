aa22823576129a84c9d119b45b128313ab174aa9
eastclo <goldeastcloud@gmail.com>
2019-12-11 22:51:02 +0900

    git push hotfix ExecutionPush.java -> RemoteList json파일 관리에 따른 내부 코드 수정

--- GitPractice/src/Controller/ExecutionPush.java
+++ GitPractice/src/Controller/ExecutionPush.java
@@ -25,48 +25,38 @@ public class ExecutionPush {
 	
 	public boolean executeCommand(String[] parameter){	
 		if(parameter.length==2) {
-			try {
-				searchRemoteList(parameter[0]); //origin에 접근
-			} catch (IOException | ParseException e) {
-				e.printStackTrace();
-			}	
-			
-			if(url == null) {	//예외처리
-				JOptionPane.showMessageDialog(null,"원격 저장소'"+parameter[0]+"'가 지정되지 않았습니다.", "명령어 입력 오류", JOptionPane.ERROR_MESSAGE);
-				return false;	
-			}
-			searchRemoteRepository();
-			
-			if(!remote.exists()) {	//예외처리
-				JOptionPane.showMessageDialog(null,"지정되지 않은 원격 저장소입니다.", "명령어 입력 오류", JOptionPane.ERROR_MESSAGE);
-				return false;
-			}
-			File localBranch = new File(currentWorkspace, parameter[1]);
-			
-			if(!localBranch.exists()) {	//예외처리
-				JOptionPane.showMessageDialog(null,"로컬저장소에 '"+parameter[1]+"' 브랜치가 존재하지 않습니다.", "명령어 입력 오류", JOptionPane.ERROR_MESSAGE);
-				return false;
-			}
+			String remoteName = Model.CurrentLocation.getRemoteList().get(parameter[0]);
+			if(remoteName != null) {
+				File localBranch = new File(currentWorkspace, parameter[1]);
 				
-			//백업
-			Model.FileOperation.makeBackup(remote);
-			
-			File remoteBranch = new File(remote,parameter[1]);
-			if(!remoteBranch.exists()) {
-				remoteBranch.mkdir();
-				Model.FileOperation.copyFileAll(localBranch, remoteBranch);
-			} else {	//원격 저장소에 푸시한 내역이 있으면 업데이트 되지 않은 커밋만 푸시함
-				File commitFolder = new File(localBranch,"commit");
-				String[] localCommitList = commitFolder.list();
-				for(String localCommit : localCommitList) {
-					File commit = new File(remoteBranch, localCommit);
-					if(!commit.exists()) {
-						commit.mkdir();
-						Model.FileOperation.copyFileAll(new File(commitFolder, localCommit), commit);
+				if(!localBranch.exists()) {	//예외처리
+					JOptionPane.showMessageDialog(null,"로컬저장소에 '"+parameter[1]+"' 브랜치가 존재하지 않습니다.", "명령어 입력 오류", JOptionPane.ERROR_MESSAGE);
+					return false;
+				}
+				remote = new File("."+File.separator+"GitHub",remoteName);
+				//백업
+				Model.FileOperation.makeBackup(remote);
+				
+				File remoteBranch = new File(remote,parameter[1]);
+				if(!remoteBranch.exists()) {
+					remoteBranch.mkdir();
+					Model.FileOperation.copyFileAll(localBranch, remoteBranch);
+				} else {	//원격 저장소에 푸시한 내역이 있으면 업데이트 되지 않은 커밋만 푸시함
+					File commitFolder = new File(localBranch,"commit");
+					String[] localCommitList = commitFolder.list();
+					for(String localCommit : localCommitList) {
+						File commit = new File(remoteBranch, localCommit);
+						if(!commit.exists()) {
+							commit.mkdir();
+							Model.FileOperation.copyFileAll(new File(commitFolder, localCommit), commit);
+						}
 					}
 				}
+				return true;
+			}else {//리모트 입력 에러
+				JOptionPane.showMessageDialog(null,"리모트 저장소 '"+parameter[0] +"'이 존재하지 않습니다.", "명령어 입력 오류", JOptionPane.ERROR_MESSAGE);
+				return false;
 			}
-			return true;
 		} else {	//인자는 반드시 2개가 와야 한다.
 			JOptionPane.showMessageDialog(null, "잘못된 명령어입니다.", "명령어 입력 오류", JOptionPane.ERROR_MESSAGE);
 			return false;
@@ -74,56 +64,11 @@ public class ExecutionPush {
 	}
 	
 	public boolean cancelCommand(String[] parameter) {
-		try {
-			searchRemoteList(parameter[0]);
-		} catch (IOException | ParseException e) {
-			e.printStackTrace();
-		}
-		searchRemoteRepository();
+		String remoteName = Model.CurrentLocation.getRemoteList().get(parameter[0]);
+		remote = new File("."+File.separator+"GitHub",remoteName);
 		
-		//복원
+		//복원	
 		Model.FileOperation.loadBackup(remote);
 		return true;
-	}
-		
-	/*입력한 remote 값을 토대로 url을 읽어오는 메소드*/
-	private void searchRemoteList(String remote) throws IOException, ParseException {
-		String url = null;
-		JSONParser parser = new JSONParser();
-		CommitArray json = new CommitArray();
-		
-		if(remote == null)	//리모트 입력값이 없다면 default는 origin
-			remote = new String("origin");
-		try {
-			FileReader rd = new FileReader(new File(currentWorkspace.getPath()+sep+".git", "RemoteList.ini"));
-			Object obj = parser.parse(rd);
-			JSONArray jsonArray = (JSONArray)obj;
-			Iterator<JSONObject> iterator =jsonArray.iterator();
-			
-			while(iterator.hasNext()) {
-				//RemoteList.ini 파일에서 remote 입력 값이 같으면 url 읽어오기
-				JSONObject jsonObject = (JSONObject)iterator.next();
-				if(remote.equals(String.valueOf(jsonObject.get(json.remoteKey))))	
-					url = String.valueOf(jsonObject.get(json.urlKey));
-			}
-			rd.close();
-		} catch (FileNotFoundException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
-		}
-		this.url = url;
-	}
-	
-	/*읽어온 url을 토대로 해당 url을 가진 레포지토리의 경로를 저장하는 메소드 */
-	private void searchRemoteRepository() {
-		Model.RepositoryList repo = new Model.RepositoryList();
-		String[] repoList = repo.getRepoList();
-		String repoPath = null;
-		
-		for(String remote : repoList) {
-			repoPath = new String("."+sep+"GitHub"+sep+remote);
-			if(url.equals(Model.FileOperation.getFileReadData(repoPath, "address.txt")))
-				this.remote = new File(repoPath);
-		}
 	}
 }
